519b5ae016a20b8cd779ba7c5a1fb121555fa5cb
Jay Goss <jayspgoss@gmail.com>
2018-11-30 20:01:05 -0500

    table workign

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,11 +2,12 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table');
 
+//establish my mysql connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "asdkasd",
+  password: "",
   database: "bamazon"
 });
 
@@ -15,8 +16,7 @@ connection.connect(function(err) {
   showProducts();
 });
 
-//switch function to select search option
-//creating a table
+//creating a table using cli table
 function showProducts() {
 connection.query('SELECT item_id, product_name, price FROM products', function(err, res) {
 var table = new Table({
@@ -24,22 +24,22 @@ var table = new Table({
 , colWidths: [30, 30, 30]
 });
 
-// table is an Array, so you can `push`, `unshift`, `splice` and friends
-table.push(
-  ['First value', 'Second value']
-, ['First value', 'Second value']
-);
+// console.log(res);
+for (i = 0; i < res.length; i++) {
+  table.push([res[i].item_id, res[i].product_name, res[i].price])
+};
 console.log(table.toString());
-}
-});
+})
+};
 
+//without timeout the question runs before the table and looks bad
+setTimeout(function makePurchase() {
+    inquirer
+      .prompt({
+        name: "startQ",
+        type: "input",
+        message: "What product would you like to buy?",
+    })
+}, 500);
 
-// function start() {
-//     inquirer
-//       .prompt({
-//         name: "startQ",
-//         type: "input",
-//         message: "What product would you like to buy?",
-//     })
-// };
 
